4661706c1a0789374e5428a01a2faf50a1b9ff57
UrePu <darkin31313@gmail.com>
2024-12-11 17:32:53 +0900

    4

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-function ageCheck(age) {
-  if (age >= 18) {
-    console.log("당신은 투표를 할 수 있습니다😊");
-  } else console.log("당신은 투표를 하기에 너무 어렵니다😐");
-}
-ageCheck(17);
+// 1부터 100까지의 합을 계산하는 프로그램
+let sum = 0;
 
-ageCheck(18);
-a;
+for (let i = 1; i <= 100; i++) {
+  sum += i; // i를 계속 더함
+}
+
+console.log("1부터 100까지의 합은:", sum);
